5a96e8521950eb2ff61a81b40b3d5cfa4c3ef8ef
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2020-12-29 17:43:47 +0100

    added api stage deployment

---
+++ ci/deploy_api_gateway_stage.py
@@ -0,0 +1,24 @@
+import click
+
+from ci.external_cmd import ExternalCmd
+
+
+@click.command()
+@click.option("--stack-name", help="Name of the stack")
+@click.option("--stage-name", help="Name of the api stage")
+def deploy(stack_name, stage_name):
+    click.echo(f"Fetching REST API id from {stack_name} stack...")
+    stack_description = ExternalCmd.run_and_parse_json(
+        f"aws cloudformation describe-stacks --stack-name {stage_name}"
+    )
+    api_id = stack_description["Outputs"]["RestApiId"]
+    click.echo(f"API id is {api_id}")
+    click.echo(f"Deploying {stage_name} stage...")
+    ExternalCmd.run(
+        f"aws apigateway create-deployment --rest-api-id {api_id} --stage-name {stage_name}"
+    )
+    click.echo("Done")
+
+
+if __name__ == "__main__":
+    deploy()
